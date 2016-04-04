defa82b00b1602ba86bd1865df52dee54961b372
Robbie Netzorg <robbizorg@gmail.com>
2016-04-04 14:49:29 -0400

    Added Rudimentary HTML

--- test.js
+++ test.js
@@ -1,7 +1,38 @@
-var aba = "Hi hi, technically this should should be be different.";
 
-var ling = Ling(aba);
-console.log(ling.count());
-console.log(ling.vocabComplexity());
-console.log(ling.uniqueCount());
-console.log(ling.unique);
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	document.getElementById("submit").addEventListener("click", function(event) {
+		if (document.getElementById("input").value.trim() == "") {
+			var nope = document.createElement("p");
+			nope.className += "result";
+			nope.appendChild(document.createTextNode("Please Enter Something"));
+
+			var result = document.getElementById("results");
+			result.appendChild(nope);
+			result.appendChild(document.createElement("br"));
+		
+		} else {
+			console.log(document.getElementById("input").value);
+			var ling = Ling(document.getElementById("input").value);
+
+			var result = document.getElementById("results");
+
+			var count = document.createElement("p");
+			count.className += "result";
+			count.appendChild(document.createTextNode("Words: " + ling.count()));
+
+			var complexity = document.createElement("p");
+			complexity.className += "result";
+			complexity.appendChild(document.createTextNode("Complexity: " + ling.vocabComplexity()));
+
+			result.appendChild(count);
+			result.appendChild(document.createElement("br"));
+			result.appendChild(complexity);
+			result.appendChild(document.createElement("br"));
+
+			document.getElementById("input").value = "";			
+		}
+
+
+	})
+});
